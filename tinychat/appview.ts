b000e668c9d40cc7f7ff3825f81c3cb666fb1606
Philip Nuzhnyi <philip.nuzhnyy@gmail.com>
2025-01-14 12:12:10 +0000

    try db activation

--- tinychat/appview.ts
+++ tinychat/appview.ts
@@ -8,7 +8,7 @@ import { createMiddleware } from "hono/factory";
 import { TinychatOAuthClient } from "tinychat/oauth.ts";
 import { Agent } from "@atproto/api";
 import { TinychatAgent } from "tinychat/utils.ts";
-// import { getDatabase } from "tinychat/db.ts";
+import { getDatabase } from "tinychat/db.ts";
 // based on https://docs.deno.com/examples/chat_app_tutorial/
 
 export default class ChatServer {
@@ -115,8 +115,8 @@ app.get(
   }),
 );
 export const runAppView = () => {
-  // const db = getDatabase();
-  // console.log("Starting appview with db", db);
+  const db = getDatabase();
+  console.log("Starting appview with db", db);
 
   // Cleanup function
   const cleanup = () => {
